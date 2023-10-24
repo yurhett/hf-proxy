9d2aa0cf461ecb3e0b311eacfbe8ffbc7236045b
yulonger's MacBook Pro 14 <yulonger@outlook.com>
2023-10-24 20:43:20 +0800

    inital commit

---
+++ main.ts
@@ -0,0 +1,67 @@
+const target = 'https://huggingface.co'
+const cdn_target = 'https://cdn-lfs.huggingface.co'
+const cdn_proxy = '<your cdn-lfs proxy address>'
+const allowMethods = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+const allowHeaders = [
+  'Accept',
+  'Accept-Encoding',
+  'Authorization',
+  'Content-Type',
+  'Origin',
+  'User-Agent',
+]
+
+const exposeHeaders = [
+  'Content-Length',
+  'Content-Type',
+  'Date',
+  'Server',
+]
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  const method = request.method
+
+  if (!allowMethods.includes(method)) {
+    return new Response('Method Not Allowed', { status: 405 })
+  }
+
+  const proxyRequest = new Request(target + url.pathname + url.search, request)
+  proxyRequest.headers.set('Host', target)
+  proxyRequest.headers.set('Referer', target)
+
+  let response = await fetch(proxyRequest)
+
+  if (response.status === 302) {
+    const location = response.headers.get('Location')
+    if (location && location.startsWith(cdn_target)) {
+      response = new Response(response.body, response)
+      response.headers.set(
+        'Location',
+        location.replace(cdn_target, cdn_proxy)
+      )
+    }
+  }
+
+  const proxyResponse = new Response(response.body, response)
+  proxyResponse.headers.set('Access-Control-Allow-Origin', url.origin)
+  proxyResponse.headers.set(
+    'Access-Control-Allow-Methods',
+    allowMethods.join(',')
+  )
+  proxyResponse.headers.set(
+    'Access-Control-Allow-Headers',
+    allowHeaders.join(',')
+  )
+  proxyResponse.headers.set(
+    'Access-Control-Expose-Headers',
+    exposeHeaders.join(',')
+  )
+
+  return proxyResponse
+}
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
